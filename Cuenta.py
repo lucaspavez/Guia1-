a870018a3c4a208de7629f3747b7faa1e08865e4
lucaspavez <38229759+lucaspavez@users.noreply.github.com>
2020-05-18 19:39:13 -0400

    Add files via upload

--- Cuenta.py
+++ Cuenta.py
@@ -1,18 +1,76 @@
-
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
 
 class Cuenta():
 	
 	
-	def __init__(self,nombre, numero_cuenta, saldo):
+	def __init__(self):
 		
-		self.nombre = nombre
-		self.numero_cuenta = numero_cuenta
-		self.saldo = saldo
+		self.__nombre = ""
+		self.__numero_cuenta = 0
+		self.saldo = 0.0
+		
+		
+	def transferencia(self,monto,nombre):
+		if (monto < self.saldo):
+			self.saldo = self.saldo - monto
+			print ("----------------------------------------------------")
+			print ("se ha transferido {0:.2f} a nombre de {1}".format(monto,
+																	  nombre))
+			print ("Ahora su cuenta dispone de {0:.2f}".format(self.saldo))
+			print ("----------------------------------------------------")
+			True
+		else:
+			print ("Su tarjeta no dispone de ese saldo")
+			False
+			
+	def ingreso(self,monto):
+		if (0 < monto):
+			self.saldo = self.saldo + monto
+			print ("--------------------------------------------")
+			print ("Ahora su tarjeta dispone de {0:.2f}$ de saldo".format(self.saldo))
+			print ("--------------------------------------------")
+			True
+		else:
+			print ("ingrese un valor positivo porfavor ")
+			False
+		
+	def giro(self,monto):
+		if (monto < self.saldo):
+			if (0 < monto):
+				self.saldo = self.saldo - monto
+				print ("---------------------------------------------------------------")
+				print ("Ahora usted dispone de {0:.2f}$ saldo en su tarjeta".format(self.saldo))
+				print ("---------------------------------------------------------------")
+				
+			else:
+				("Ingrese una cantidad positiva")
+		else:
+			print ("Usted no dispone de saldo suficiente")
+			
+	def mostrar_cuenta(self):
+		print ("---------------Tarjeta registrada-----------------------")
+		print ("Nombre: {0}, Numero de cuenta: {1}, Saldo: {2:.2f}".format(self.__nombre,
+																		self.__numero_cuenta,
+																		  self.saldo))
+		print ("--------------------------------------------------------")
 		
 	def getSaldo(self):
 		return self.saldo
 		
 	def setSaldo(self, nuevosaldo):
 		self.saldo = nuevosaldo
+		
+	def get__nombre(self):
+		return self.__nombre
+		
+	def set__nombre(self,nombre):
+		self.__nombre = nombre
+			
+	def get__numero_cuenta(self):
+		return self.__numero_cuenta
+		
+	def set__numero_cuenta(self, numero):
+		self.__numero_cuenta = numero 
 		
 	
--- mainCuenta.py
+++ mainCuenta.py
@@ -1,8 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
 from Cuenta import Cuenta
 
-nombre = raw_input("ingrese su nombre: ")
+
+nombre = input("ingrese su nombre: ")
 numero_cuenta = int(input("ingrese el numero de su cuenta: "))
 saldo = float(input("ingrese su saldo (es de tipo flotante): "))
 
-Cuenta1 = Cuenta(nombre, numero_cuenta,saldo)
+Cuenta1 = Cuenta()
+Cuenta1.set__nombre(nombre)
+Cuenta1.set__numero_cuenta(numero_cuenta)
+Cuenta1.setSaldo(saldo)
+
+Cuenta1.mostrar_cuenta()
+while True:
+	print ("")
+	print ("     Menu      ")
+	print ("1. Realizar giro")
+	print ("2. Ingresar dinero a su tarjeta")
+	print ("3. Transferencia a terceros")
+	print ("4. Salir del cajero")
+	print ("")
+	opcion = int(input("Seleccione la opcion que desea ejecutar: "))
+	print ("")
+	if (opcion == 1):
+		monto = float(input("¿Cuanto desea retirar de su tarjeta?: "))
+		Cuenta1.giro(monto)
+	if (opcion == 2):
+		monto = float(input("¿Cuanto desea ingresar en su tarjeta?: "))
+		Cuenta1.ingreso(monto)
+	if (opcion == 3): 	
+		monto = float(input("¿Cuanto desea transferir?: "))
+		nombre2 = input("A nombre de quien desea transferir: ")
+		Cuenta1.transferencia(monto,nombre2)
+	if (opcion == 4):
+		print ("Gracias por preferir nuestros servicios")
+		print ("Que tenga un buen dia")
+		break
+
+		
 
